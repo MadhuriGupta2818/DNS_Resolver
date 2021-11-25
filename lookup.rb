145682393db1d9429f16e9f206715de9a49e095c
MadhuriGupta2818 <85929203+MadhuriGupta2818@users.noreply.github.com>
2021-11-25 16:18:12 +0530

    Update lookup.rb

--- lookup.rb
+++ lookup.rb
@@ -1,12 +1,26 @@
 def get_command_line_argument
+    # ARGV is an array that Ruby defines for us,
+    # which contains all the arguments we passed to it
+    # when invoking the script from the command line.
+    # https://docs.ruby-lang.org/en/2.4.0/ARGF.html
     if ARGV.empty?
       puts "Usage: ruby lookup.rb <domain>"
       exit
     end
     ARGV.first
   end
-domain = get_command_line_argument 
-dns_raw = File.readlines("zone")
+  
+  # `domain` contains the domain name we have to look up.
+  domain = get_command_line_argument
+  
+  # File.readlines reads a file and returns an
+  # array of string, where each element is a line
+  # https://www.rubydoc.info/stdlib/core/IO:readlines
+  dns_raw = File.readlines("zone")
+  
+  # ..
+  # ..
+  # FILL YOUR CODE HERE
   def parse_dns(dns_raw)
         hash= {}
         dns_raw.each do |record|
@@ -17,6 +31,7 @@ dns_raw = File.readlines("zone")
         end
     return hash
     end
+
 def resolve(dns_records, lookup_chain, domain)
       if lookup_chain[-1].is_a? String 
         lookup_chain.push(dns_records[domain])
@@ -30,6 +45,11 @@ def resolve(dns_records, lookup_chain, domain)
     end
     return lookup_chain[0..-2]
     end
+  # ..
+  
+  # To complete the assignment, implement `parse_dns` and `resolve`.
+  # Remember to implement them above this line since in Ruby
+  # you can invoke a function only after it is defined.
   dns_records = parse_dns(dns_raw)
   lookup_chain = [domain]
   lookup_chain = resolve(dns_records, lookup_chain, domain)
