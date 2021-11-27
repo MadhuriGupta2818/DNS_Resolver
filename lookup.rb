45c99f91fb1f56d3e92a984933513f9c1fc929f3
MadhuriGupta2818 <85929203+MadhuriGupta2818@users.noreply.github.com>
2021-11-27 11:02:28 +0530

    Add files via upload

---
+++ lookup.rb
@@ -0,0 +1,40 @@
+def get_command_line_argument
+    if ARGV.empty?
+      puts "Usage: ruby lookup.rb <domain>"
+      exit
+    end
+    ARGV.first
+  end
+  # `domain` contains the domain name we have to look up.
+  domain = get_command_line_argument
+dns_raw = File.readlines("zone")
+    # FILL YOUR CODE HERE
+  def parse_dns(dns_raw)
+        hash= {}
+        dns_raw.each do |record|
+            record = record.split(",")
+            if record.length() > 1 && record[0] != "# RECORD TYPE"
+                hash[record[1].strip()] = record[2].strip()
+            end
+        end
+    return hash
+    end
+
+def resolve(dns_records, lookup_chain, domain)
+      if lookup_chain[-1].is_a? String 
+        lookup_chain.push(dns_records[domain])
+        resolve(dns_records, lookup_chain,lookup_chain[-1])
+      end
+    
+    res = lookup_chain[0..-2]
+    if res.length<2
+      puts "Error: record not found for " + lookup_chain[0]
+      exit
+    end
+  return lookup_chain[0..-2]
+ end
+
+  dns_records = parse_dns(dns_raw)
+  lookup_chain = [domain]
+  lookup_chain = resolve(dns_records, lookup_chain, domain)
+   puts lookup_chain.join(" => ")
